9ac510e4f79b8443519e7623797bc1503f3ce279
Jesús Raúl <1269884@senati.pe>
2023-04-03 17:56:11 -0500

    Base de datos terminado

---
+++ database/basedatos.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE senati;
+ USE senati; 
+ 
+  CREATE TABLE cursos(
+idcurso 	INT AUTO_INCREMENT PRIMARY KEY,
+nombrecurso 	VARCHAR(50) 	NOT NULL,
+especialidad 	VARCHAR(70)	NOT NULL,
+complejidad 	CHAR(1) 	NOT NULL DEFAULT 'B',
+fechainicio 	DATE 		NOT NULL,
+precio 		DECIMAL(7,2) 	NOT NULL,
+fechacreacion 	DATETIME 	NOT NULL DEFAULT NOW(),
+fechaupdate   	DATETIME 	NULL,
+estado		CHAR(1)		NOT NULL DEFAULT '1'
+ )ENGINE = INNODB;
+ 
+ INSERT INTO cursos (nombrecurso,especialidad,complejidad,fechainicio,precio) VALUES
+	('Java','ETI','M','2023-05-10',180),
+	('Desarrollo Web','ETI','B','2023-05-20',190),
+	('Excel financiero','Administracion','A','2023-04-14',250),
+	('ERP SAP','Administracion','A','2023-05-11',400),
+	('Inventor','Mecanica','M','2023-05-29',380);
+
+SELECT * FROM cursos;
+
+-- STORE PROCEDURE
+-- Un precedimiento almacenado es un PROGRAMA que se ejecuta desde el
+-- motot de BD y que permite recibir valores de entrada, reaizar
+-- diferentes tipos de calculo y entregar un salida
+
+DELIMITER $$ -- El $$ hace referencia fin del ciclo
+CREATE PROCEDURE spu_cursos_listar()
+BEGIN
+	SELECT 	idcurso,
+		nombrecurso,
+		especialidad,
+		complejidad,
+		fechainicio,
+		precio
+	 FROM cursos
+	 WHERE estado = '1'
+	 ORDER BY idcurso DESC;
+END $$
+CALL spu_cursos_listar()
+
+
+-- PAR REGISTRAR CURSOS
+DELIMITER $$
+CREATE PROCEDURE spu_registrar_cursos(
+	IN _nombrecurso 	VARCHAR(50),
+	IN _especialidad 	VARCHAR(70),
+	IN _complejidad 	CHAR(1),
+	IN _fechainicio 	DATE,
+	IN _precio			DECIMAL(7,2)
+)
+BEGIN
+	INSERT INTO cursos(nombrecurso,especialidad,complejidad,fechainicio,precio)
+	VALUES (_nombrecurso,_especialidad,_complejidad,_fechainicio,_precio);
+END $$
+
+CALL spu_registrar_cursos('Python','ETI','B','2023-05-10','150');
+
+CALL spu_cursos_listar()
