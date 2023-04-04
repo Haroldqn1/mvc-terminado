3adf62e892c238be7565485df7e78bd5814d2185
Jesús Raúl <1269884@senati.pe>
2023-04-04 10:34:27 -0500

    Metodo listar terminado en curso

--- models/curso.php
+++ models/curso.php
@@ -7,17 +7,54 @@ require_once "./conexion.php"
 class Curso extends Conexion{
 
   //Objeto que almacena la conexion que viene desde el padre(Conexion)
+  //y la comparira con todoso los metodos(CRUD+)
   private $accesoBD;
 
-  //Constructor
-  public function __CONSTRUCTOR(){
-
+  //Constructor, INICIALIZAR
+  public function __CONSTRUCT(){
+    $this->accesoBD = parent::getConexion();
   }
 
   //Metodo listar cursos
   public function listarCursos(){
+    try{
+      //1:PREPARAMOS LA CONSULTA
+      $consulta = $this->accesoBD->prepare("CALL spu_cursos_listar()");
+      //2:EJECUTAMOS LA CONSULTA
+      $consulta->execute();
+      //3:DEVOLVEMOS LA CONSULTA(array asociativo)
+      return $consulta->fechAll(PDO::FERCH_ASSOOC);
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+  //----------------------
+  public function registrarCurso(){
+    try{
 
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+  //------------------------------
+  public function eliminarCurso(){
+    try{
 
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+  //---------------------------
+  public function actualizarCurso(){
+    try{
+
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
   }
 }
 
